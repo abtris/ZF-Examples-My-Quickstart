4c613c88ce54d13e188474ac746de784e226c24e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-11 12:17:40 +0100

    Add methods to Models

--- application/models/DbTable/Comments.php
+++ application/models/DbTable/Comments.php
@@ -5,6 +5,59 @@ class Application_Model_DbTable_Comments extends Zend_Db_Table_Abstract
 
     protected $_name = 'comments';
 
+    public function getCommentbyPost($id)
+    {
+        $id = (int) $id;
+        $row = $this->fetchAll('parent IS NULL AND post = ' . $id);
+        if (!$row) {
+            throw new Exception("Count not find row $id");
+        }
+        return $row->toArray();
+    }
 
+    public function getReply($id)
+    {
+        $id = (int) $id;
+        $row = $this->fetchAll('parent = ' . $id);
+        if (!$row) {
+            throw new Exception("Count not find row $id");
+        }
+        return $row->toArray();
+    }
+
+    public function getComment($id)
+    {
+        $id = (int) $id;
+        $row = $this->fetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Count not find row $id");
+        }
+        return $row->toArray();
+    }
+
+    public function addComment($values)
+    {
+        $data = array(
+            'comment' => $values['comment'],
+            'post' => $values['post'],
+            'parent' => empty($values['post']) ? new Zend_Db_Expr('NULL') : $values['parent']
+        );
+        $this->insert($data);
+    }
+
+    public function updateComment($values, $id)
+    {
+        $data = array(
+            'comment' => $values['comment'],
+            'post' => $values['post'],
+            'parent' => $values['parent']
+        );
+        $this->update($data, 'id = ' . (int) $id);
+    }
+
+    public function deleteComment($id)
+    {
+        $this->delete('id =' . (int) $id);
+    }
 }
 
--- application/models/DbTable/Posts.php
+++ application/models/DbTable/Posts.php
@@ -2,9 +2,40 @@
 
 class Application_Model_DbTable_Posts extends Zend_Db_Table_Abstract
 {
-
+    
     protected $_name = 'posts';
-
-
+    
+    public function getPost($id)
+    {
+        $id = (int) $id;
+        $row = $this->fetchRow('id = ' . $id);
+        if (!$row) {
+            throw new Exception("Count not find row $id");
+        }
+        return $row->toArray();
+    }
+    
+    public function addPost($values)
+    {
+        $data = array(
+            'title' => $values['title'],
+            'content' => $values['content'],
+        );
+        $this->insert($data);
+    }
+    
+    public function updatePost($values, $id)
+    {
+        $data = array(
+            'title' => $values['title'],
+            'content' => $values['content'],
+        );
+        $this->update($data, 'id = ' . (int) $id);
+    }
+    
+    public function deletePost($id)
+    {
+        $this->delete('id =' . (int) $id);
+    }
 }
 
